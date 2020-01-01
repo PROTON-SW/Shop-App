0551d40fcb9fedf565092ec3845e7d1c0530ebbd
prrag <pavlyremon@gmail.com>
2020-01-01 21:52:05 +0200

    last edit

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:provider/provider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shop_app/providers/cart.dart';
 
 import './providers/products-provider.dart';
 import './screens/product-details-screen.dart';
@@ -20,27 +21,34 @@ class MyApp extends StatelessWidget {
         );
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      builder: (ctx) => Products(),
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.purple,
-          accentColor: Colors.deepOrange,
-          canvasColor: Color.fromRGBO(255, 254, 229, 1),
-          fontFamily: 'Lato',
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: Products(),
         ),
-        routes: {
-          ProductsOverviewScreen.routeName: (ctx) => ProductsOverviewScreen(
-                appBar: appBar,
-              ),
-          ProductDetailsScreen.routeName: (ctx) => ProductDetailsScreen(
-              ),
-        },
-        home: ProductsOverviewScreen(
-          appBar: appBar,
+        ChangeNotifierProvider.value(
+          value: Cart(),
         ),
-      ),
+      ],
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.purple,
+            accentColor: Colors.deepOrange,
+            canvasColor: Color.fromRGBO(255, 254, 229, 1),
+            fontFamily: 'Lato',
+          ),
+          routes: {
+            ProductsOverviewScreen.routeName: (ctx) => ProductsOverviewScreen(
+                  appBar: appBar,
+                ),
+            ProductDetailsScreen.routeName: (ctx) => ProductDetailsScreen(
+                ),
+          },
+          home: ProductsOverviewScreen(
+            appBar: appBar,
+          ),
+        ),
     );
   }
 }
